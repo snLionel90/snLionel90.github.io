59f8726d443c3e5c0012d65dd978a262596eb528
Lionel <leonjuman@gmail.com>
2022-02-13 20:27:54 +0100

    responsive??

--- navigation/scripts/app.word_container.js
+++ navigation/scripts/app.word_container.js
@@ -1,4 +1,30 @@
-const propts = [
-    ["Hola" ,"Buenos Dias", "Buenas Noches", "Buenas Tardes"],
-    ["Cual es su nombre", " Encantado de conocerle", " "]
-]
+const prompts = [
+    ["hola", "hey", "hello", "buenos dias", "buenas tardes"],
+    ["como estas", "cuentame algo", "how are things"],
+    ["Que mas te gusta hacer", "cual seria tu rango salarial", "Que tal?"],
+    ["que edad tienes","a que te dedicas en tu tiempo libre"],["Que nivel de ingles tienes"],
+    ["Quien eres", "como te llamas", "hablame sobre tus estudios", "Cuentame un poco sobre tu experiencia laboral"],
+    ["Tienes experiencia en Frameworks", "Que tipo de tecnologias usas/utilizas"],
+    ["Que frameworks utilizas","molan tus dibujos, los haces tu"],
+    ["NodeJs","AngularJs","VueJs","Cuanto tiempo"],
+    ["Como te llamas","tu nombre","me suena tu nombre","cual es tu nombre","describete"]
+  ]
+  
+  // Possible responses, in corresponding order
+  
+  const replies = [
+    ["hola", "hey", "hello", "buenos dias", "buenas tardes"],
+    ["Mi nombre es Lionel Sanchez","Me ha Programado Lionel Sachez", "Soy Not-Chan"],
+    [""]
+  ]
+  
+  // Random for any other user input
+  
+  const alternative = [
+    "Genial",
+    "Vale...",
+    "fenomenal...",
+    "Perfeco",
+    "Escuchando peticiones...",
+    "Lo siento no eschuché bien, me lo puede repetir? :/"
+  ]
