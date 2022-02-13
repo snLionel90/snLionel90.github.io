bcada4e17df8c56d1edf10d1a9b994974883055b
Lionel <leonjuman@gmail.com>
2022-02-13 22:18:20 +0100

    bot activated2

--- navigation/scripts/app.responsive.js
+++ navigation/scripts/app.responsive.js
@@ -1,3 +1,36 @@
+const prompts = [
+    ["hola", "hey", "hello", "buenos dias", "buenas tardes"],
+    ["que tal", "cuentame algo", "how are things"],
+    ["Que mas te gusta hacer", "cual seria tu rango salarial", "Que tal?"],
+    ["que edad tienes","a que te dedicas en tu tiempo libre","Que nivel de ingles tienes"],
+    ["Quien eres", "como te llamas", "hablame sobre tus estudios", "Cuentame un poco sobre tu experiencia laboral"],
+    ["Tienes experiencia en Frameworks", "Que tipo de tecnologias usas/utilizas"],
+    ["Que frameworks utilizas","molan tus dibujos, los haces tu"],
+    ["NodeJs,AngularJs,VueJs,por Cuanto tiempo"]
+  ]
+  
+  // Possible responses, in corresponding order
+  
+  const replies = [
+    ["hola", "hey", "hello", "buenos dias", "buenas tardes"],
+    ["Bien","Me ha Programado Lionel Sanchez", "Soy Bot-Chan"],
+    ["Desarrollar sitios web, Programar aplicaciones, estar con mi familia, buscar trabajo", "entre 16mil y 22mil"," bien"],
+    ["30", "Estar con los demas, salir y crear contenido","mi nivel de ingles es avanzado C1"],
+    ["Soy humano","mi nombre es Lionel Sanchez"," Estoy graduado en 3 FP, Grado medio en imagen y sonido, grado medio en sistemas y grado superior en desarrollo","5 años en desarrollo"],
+    ["si, mas de 4 años","utilizo tecnologias de lentejas con shorisho"],
+    ["Node Js, AngularJS, SpringBoot, JUnit, REST,API, Docker, Python3","Si, los hago con Medibang Paint Pro"],
+    ["Correcto, durante mas de 3 años"]
+  ]
+  
+  // Random for any other user input
+  
+  const alter = [
+    "Escuchando peticiones...",
+    "Lo siento no eschuché bien, me lo puede repetir? :/"
+  ]
+
+//seccion donde se plasma todo en el documento HTML
+
 document.addEventListener("DOMContentLoaded", () => {
     const inputField = document.getElementById("input");
     inputField.addEventListener("keydown", (e) => {
